b61a108e5956a402615d0af12f04e385106e8f44
linouk23 <linouk23@gmail.com>
2018-08-26 13:35:24 -0400

    Refactor solution: 52

--- 52.cpp
+++ 52.cpp
@@ -7,31 +7,30 @@ using namespace std;
 class Solution {
 private:
     int n, result = 0;
-    void helper(int cur_row, deque<bool> rows, deque<bool> cols, deque<bool> dL, deque<bool> dR) {
-        if (cur_row == n) {
+    void generate(int row, vector<bool> &cols, vector<bool> &ldiag, vector<bool> &rdiag) {
+        if (row == n) {
             ++result;
             return;
         }
+        
         for (int col = 0; col < n; ++col) {
-            int indL = cur_row + col;
-            // Using the 1st col:
-            int indR = n - 1 - cur_row + col;
-            if (rows[cur_row] || cols[col] || dL[indL] || dR[indR]) {
-                continue;
-            }
-            rows[cur_row] = cols[col] = dL[indL] = dR[indR] = true;
-            helper(cur_row + 1, rows, cols, dL, dR);
-            rows[cur_row] = cols[col] = dL[indL] = dR[indR] = false;
+            bool isCellUnderAttack = cols[col] || ldiag[row + col] || rdiag[n - row + col - 1];
+            if (isCellUnderAttack) { continue; }
+            
+            cols[col] = ldiag[row + col] = rdiag[n - row + col - 1] = true;
+            
+            generate(row + 1, cols, ldiag, rdiag);
+            
+            cols[col] = ldiag[row + col] = rdiag[n - row + col - 1] = false;
         }
     }
 public:
     int totalNQueens(int n) {
         this->n = n;
-        deque<bool> rows(n, false);
-        auto cols = rows;
-        deque<bool> dL(2 * n, false);
-        auto dR = dL;
-        helper(0, rows, cols, dL, dR);
+        vector<bool> cols(n, false);
+        vector<bool> ldiag(2 * n, false);
+        auto rdiag = ldiag;
+        generate(0, cols, ldiag, rdiag);
         return result;
     }
 };
